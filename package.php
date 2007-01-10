ea72ec0434ff6ac6f1bed5a02211d2248ea9aae8
Laurent Laville <farell@php.net>
2007-01-10 17:37:18 +0000

    initial commit of package script generator - prepare tomorrow release
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageUpdate/trunk@226943 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ package.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * PEAR_PackageUpdate Package Script Generator
+ *
+ * Generate a new fresh version of package xml 2.0 built with PEAR_PackageFileManager 1.6.0+
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   PEAR
+ * @package    PEAR_PackageUpdate
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @copyright  2007 The PHP Group
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/PEAR_PackageUpdate
+ * @since      File available since Release 0.6.0
+ * @ignore
+ */
+
+require_once 'PEAR/PackageFileManager2.php';
+
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$packagefile = 'c:/php/pear/PEAR_PackageUpdate/package2.xml';
+
+$options = array('filelistgenerator' => 'cvs',
+    'packagefile' => 'package2.xml',
+    'baseinstalldir' => 'PEAR',
+    'simpleoutput' => true,
+    'clearcontents' => false,
+    'changelogoldtonew' => false,
+    'ignore' => array('package.php')
+    );
+
+$p2 = &PEAR_PackageFileManager2::importOptions($packagefile, $options);
+$p2->setPackageType('php');
+$p2->addRelease();
+$p2->generateContents();
+$p2->setReleaseVersion('0.6.0');
+$p2->setAPIVersion('1.0.0');
+$p2->setReleaseStability('beta');
+$p2->setAPIStability('beta');
+$p2->setNotes('* news
+- two new functions: getInstalledRelease() and getLatestRelease(), to easily
+retrieve informations (release date, notes, version, state, dependencies, ...)
+about installed and latest version available of a package.
+
+TIP: usefull if you want to alert web admin of an auto-update with all necessary informations.
+');
+
+if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
+    $p2->writePackageFile();
+} else {
+    $p2->debugPackageFile();
+}
+?>
