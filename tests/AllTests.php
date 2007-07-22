81e13a1a2c9695980cd6e6d6f8084ef70b9595e3
Laurent Laville <farell@php.net>
2007-07-22 18:34:30 +0000

    PPU Unit Tests : coverage 32.5%, first step for upcoming final stable release 1.0.0
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageUpdate/trunk@240152 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/AllTests.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * PEAR_PackageUpdate no-regression test suite
+ *
+ * PHP version 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   PEAR
+ * @package    PEAR_PackageUpdate
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @copyright  2007 Laurent Laville
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/PEAR_PackageUpdate
+ * @since      File available since Release 1.0.0
+ */
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'PEAR_PackageUpdate_AllTests::main');
+}
+
+require_once 'PHPUnit/Framework.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+chdir(dirname(__FILE__));
+
+require_once 'PEAR_PackageUpdate_TestCase_DefaultConfig.php';
+
+/**
+ * PEAR_PackageUpdate no-regression test suite
+ *
+ * Run all tests from the package root directory:
+ * #phpunit PEAR_PackageUpdate_AllTests tests/AllTests.php
+ * or
+ * #php tests/AllTests.php
+ * or for code coverage testing
+ * #phpunit --report tests/coverage PEAR_PackageUpdate_AllTests tests/AllTests.php
+ *
+ * After the code coverage test browse the index.html file in tests/coverage.
+ * The code coverage is close to 100%.
+ *
+ * @category   PEAR
+ * @package    PEAR_PackageUpdate
+ * @author     Laurent Laville <pear@laurent-laville.org>
+ * @copyright  2007 Laurent Laville
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/PEAR_PackageUpdate
+ * @since      Class available since Release 1.0.0
+ */
+
+class PEAR_PackageUpdate_AllTests
+{
+    /**
+     * Runs the test suite
+     *
+     * @return void
+     * @access public
+     * @static
+     * @since  1.0.0
+     */
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+    /**
+     * Runs the test suite
+     *
+     * @return object the PHPUnit_Framework_TestSuite object
+     * @access public
+     * @static
+     * @since  1.0.0
+     */
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('PEAR_PackageUpdate Test Suite');
+        $suite->addTestSuite('PEAR_PackageUpdate_TestCase_DefaultConfig');
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'PEAR_PackageUpdate_AllTests::main') {
+    PEAR_PackageUpdate_AllTests::main();
+}
+
+?>
