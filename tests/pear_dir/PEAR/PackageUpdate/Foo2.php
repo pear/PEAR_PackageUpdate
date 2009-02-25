146f9a111b77fe8f67a3a45379db49c50a07bb5b
Laurent Laville <farell@php.net>
2009-02-25 11:05:04 +0000

    fake PPU driver with invalid structure
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageUpdate/trunk@276422 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/pear_dir/PEAR/PackageUpdate/Foo2.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * fake PEAR_PackageUpdate driver
+ * Class does not implement required methods presentUpdate() and forceRestart()
+ */
+
+class PEAR_PackageUpdate_Foo2 extends PEAR_PackageUpdate {
+}
+?>
